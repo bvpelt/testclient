68582e477e74cf7e5707dc60b71eb895b482bb34
Bart van Pelt <bart.vanpelt@gmail.com>
2015-09-25 20:24:11 +0200

    First release

---
+++ src/main/java/nl/kadaster/geodatastore/App.java
@@ -0,0 +1,37 @@
+package nl.kadaster.geodatastore;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static Logger logger = LoggerFactory.getLogger(App.class);
+    private int failures = 0;
+    private int tests = 0;
+
+    public static void main(String[] args) {
+
+        App app = new App();
+
+        app.doTests();
+    }
+
+    private void doTests() {
+        logger.info("Start tests");
+
+        TestClient tc = new TestClient();
+
+        failures += tc.Test01();
+        tests++;
+
+        failures += tc.Test02();
+        tests++;
+
+        failures += tc.Test03();
+        tests++;
+
+        logger.info("End   tests, executed: {} with {} failures", tests, failures);
+    }
+}
