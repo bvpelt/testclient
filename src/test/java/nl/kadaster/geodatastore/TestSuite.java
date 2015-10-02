6c1718da86b8b37c1caf2b148f7265e88d1b9874
U-KADASTER-NL\peltb <peltb@vd0000147.kadaster.local>
2015-10-02 10:22:08 +0200

    Added test to get List of datasets

---
+++ src/test/java/nl/kadaster/geodatastore/TestSuite.java
@@ -0,0 +1,15 @@
+package nl.kadaster.geodatastore;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Created by peltb on 10/2/2015.
+ */
+@RunWith(Suite.class)
+@SuiteClasses({ TestJsonConverter.class })
+
+public class TestSuite {
+}
